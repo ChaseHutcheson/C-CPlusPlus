95f4990d6fee2a79cf0767c6ef5dcdf8dd7cbe70
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2024-04-24 14:08:11 -0400

    mmmm

---
+++ C Notes/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    double a = 33.1415926598708089098765;
+    double b = 3.1415926598708089098765;
+
+    printf("%lf\n", a);
+    printf("%lf", b);
+    return 0;
+
+};
